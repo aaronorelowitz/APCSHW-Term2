784859760fd72857407674728d106e2ffd1089b9
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-06 13:19:36 -0500

    Update QuickSelect.java

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -1,8 +1,12 @@
- import java.util.*;
+ 
+import java.util.*;
 
 public class QuickSelect{
 
     private Random random;
+
+
+    
     
     public void partition(int[]ary, int si, int ei){
 	int smallestopen=si;
@@ -13,8 +17,10 @@ public class QuickSelect{
 	    if ( i < si || i > ei){
 		D[i] = ary[i];}
 	}
-	int x = random.nextInt(ei - si);
+	int x = random.nextInt(ei - si + 1);
+	System.out.println(ary[x]);
 	for (int i = si; i <= ei; i++){
+	    System.out.println(Arrays.toString(D));
 	    if(ary[i] < ary[x]){
 		D[smallestopen] = ary[i];
 		smallestopen = smallestopen + 1;}
@@ -24,8 +30,14 @@ public class QuickSelect{
 	}
 	D[smallestopen] = ary[x];
 
+	 System.out.println(Arrays.toString(D));
+
+    }
+    public static void main(String[]args){
+	QuickSelect a = new QuickSelect();
+	int[]x = {1,3,5,2,4,8,6,7,9};
+	a.partition(x, 0, x.length-1);
+	
     }
 
-
 }
-
