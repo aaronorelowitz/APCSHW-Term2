2bf6d835e694d5b118af982da78d62778417d22a
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-09 13:19:40 -0400

    Update Sorts.java

--- 06QuickSelect/Sorts.java
+++ 06QuickSelect/Sorts.java
@@ -1,1 +1,77 @@
 
+import java.util.*;
+
+public class Sorts{
+
+    private Random random;
+
+
+    public int quickSelect(int[]ary, int num){
+	return quickSelectHelp(ary, 0, ary.length, num);
+    }
+    
+    public int quickSelectHelp(int[]ary, int si, int ei, int num){
+	if(ary.length != 1){
+	    int smallestopen=si;
+	    int biggestopen = ei;
+	    random = new Random();
+	    int[]D = new int[ary.length];
+	    for (int i = 0; i < ary.length; i++){
+		if ( i < si || i > ei){
+		    D[i] = ary[i];}
+	    }
+	    int x = random.nextInt(ei - si + 1);
+	    System.out.println(ary[x]);
+	    for (int i = si; i <= ei; i++){
+		System.out.println(Arrays.toString(D));
+		if(ary[i] < ary[x]){
+		    D[smallestopen] = ary[i];
+		    smallestopen = smallestopen + 1;}
+		if(ary[i] > ary[x]){
+		    D[biggestopen] = ary[i];
+		    biggestopen = biggestopen - 1;}
+	    }
+	    D[smallestopen] = ary[x];
+	    if(num < smallestopen){
+		return quickSelectHelp(D, 0, smallestopen, num);
+	    }else{return quickSelectHelp(D, smallestopen, D.length, num);}}
+    return ary[0];   	    	   
+    }
+
+
+
+
+    
+    public void partition(int[]ary, int si, int ei){
+	int smallestopen=si;
+	int biggestopen = ei;
+	random = new Random();
+	int[]D = new int[ary.length];
+	for (int i = 0; i < ary.length; i++){
+	    if ( i < si || i > ei){
+		D[i] = ary[i];}
+	}
+	int x = random.nextInt(ei - si + 1);
+	System.out.println(ary[x]);
+	for (int i = si; i <= ei; i++){
+	    System.out.println(Arrays.toString(D));
+	    if(ary[i] < ary[x]){
+		D[smallestopen] = ary[i];
+		smallestopen = smallestopen + 1;}
+	    if(ary[i] > ary[x]){
+		D[biggestopen] = ary[i];
+		biggestopen = biggestopen - 1;}
+	}
+	D[smallestopen] = ary[x];
+
+	 System.out.println(Arrays.toString(D));
+
+    }
+    public static void main(String[]args){
+	QuickSelect a = new QuickSelect();
+	int[]x = {1,3,5,2,4,8,6,7,9};
+	a.quickSelect(x, 3);
+	
+    }
+
+}
