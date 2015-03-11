657b0a43dfe2c25e6b4e9b8362f1316ac5baaaef
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-11 13:17:19 -0400

    Update MyLinkedList.java

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -7,11 +7,13 @@ public class MyLinkedList{
     public String toString(){
 	String ans = "[ ";
 	LNode x = head; 
-    while(x.getNext() != null){
-	ans = ans + x.getValue() + ", ";
-	x = x.getNext();}
-    return ans;
+	while(x.getNext() != null){
+	    ans = ans + x.getValue() + ", ";
+	    x = x.getNext();}
+	return ans;
     }
+
+    
 
 
 }
