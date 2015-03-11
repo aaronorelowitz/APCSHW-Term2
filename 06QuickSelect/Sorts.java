67a6d2794a85813923825bf5053a498510c2d9cb
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-10 20:55:04 -0400

    Update Sorts.java

--- 06QuickSelect/Sorts.java
+++ 06QuickSelect/Sorts.java
@@ -66,11 +66,78 @@ public class Sorts{
 	 System.out.println(Arrays.toString(D));
 
     }
-    public static void main(String[]args){
-	Sorts a = new Sorts();
-	int[]x = {1,3,5,2,4,8,6,7,9};
-	System.out.println(a.quickSelect(x, 9));
-	
+
+
+     public static void quickSort(int[]ary){
+	quickSortHelp(ary, 0, ary.length-1);
     }
 
-}
+    public static void quickSortHelp(int[] ary, int si, int ei){
+	if (!(si >= ei)){
+     
+	int pivot = partition2(ary, si, ei);
+	quickSortHelp(ary, si, pivot-1);
+	quickSortHelp(ary, pivot+1, ei);}
+    }
+
+    public static int partition2(int[]ary, int si, int ei){
+	int start = si;
+	int end = ei; 
+	int ri = start + (int)(Math.random()*(end-start+1));
+	int pivot = ary[ri];
+	int x,y;
+	while (si != ei){
+	    if (ary[si] == pivot){
+		x = ary[si];
+		y = ary[si + 1];
+		ary[si+1] = x;
+		ary[si] = y;
+	    }
+	    if (ary[ei] == pivot){
+		x = ary[ei];
+		y = ary[ei-1];
+		ary[ei-1] = x;
+		ary[ei] = y;
+	    }
+	    if (ary[si] > ary[ei]){
+		x = ary[si];
+		y = ary[ei];
+		ary[ei] = x;
+		ary[si] = y;
+	    }
+	    if (ary[ei] > pivot){
+		ei--;
+	    }
+	    if (ary[si] < pivot){
+		si++;
+	    }
+	}
+	return si;
+     }
+
+
+
+
+
+
+    public static void main(String[]args){
+	int[]a = {10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+	int[]b = {10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+	int[]c = {-5,17,9,3,6,1,-7,14,8,4,0,5,2,7,-2,10,16,35};
+	int[]d = {-5,17,9,3,6,1,-7,14,8,4,0,5,2,7,-2,10,16,35};
+	Sorts.quickSort(a);
+	Sorts.quickSort(c);
+	Arrays.sort(b);
+	Arrays.sort(d);
+	if (Arrays.equals(a, b)){
+	    System.out.println("True"+ Arrays.toString(a));
+	}else{
+	    System.out.println("noooooo");
+	}
+	if (Arrays.equals(c, d)){
+	    System.out.println("True"+ Arrays.toString(c));
+	}else{
+	    System.out.println("noooooo");
+	}
+    }
+    } 
