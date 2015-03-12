9b58d8faf796a53b7812663d1ffa55d1b20fd444
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-12 13:19:40 -0400

    Update MyLinkedList.java

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class MyLinkedList{
 
     public LNode head;
+    
+    public MyLinkedList(){
+	head = null;}
 
     public String toString(){
 	String ans = "[ ";
@@ -10,11 +13,26 @@ public class MyLinkedList{
 	while(x != null){
 	    ans = ans + x.getValue() + ", ";
 	    x = x.getNext();}
-	return ans.substring(0, ans.length() - 1) + "]";
+	return ans.substring(0, ans.length() - 2) + "]";
     }
 
+    public boolean add(int e){
+	LNode node = new LNode(e);
+	if(head == null){
+	    head = node;}
+	else{LNode x = head; 
+	while(x != null && x.getNext() != null){
+	    x = x.getNext();}
 
+	x.setNext(node);}
+	return true;}
     
 
+    public static void main(String[]args){
+	MyLinkedList test = new MyLinkedList();
+	System.out.println(test);
+	test.add(7);
+	System.out.println(test);
+    }
 
 }
