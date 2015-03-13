85de89280a19c27254fbba7bcc66ac1f00272644
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-13 13:16:25 -0400

    Update MyLinkedList.java

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -3,9 +3,13 @@ import java.util.*;
 public class MyLinkedList{
 
     public LNode head;
+    public LNode tail;
+    public int size;
     
     public MyLinkedList(){
-	head = null;}
+	head = null;
+	tail = null;
+	size = 0;}
 
     public String toString(){
 	String ans = "[ ";
@@ -20,18 +24,37 @@ public class MyLinkedList{
 	LNode node = new LNode(e);
 	if(head == null){
 	    head = node;}
-	else{LNode x = head; 
-	while(x != null && x.getNext() != null){
-	    x = x.getNext();}
-
-	x.setNext(node);}
+	else{tail.setNext(node);}
+	size = size + 1;
+	tail = node;
 	return true;}
     
+
+    public boolean add(int index, int e){
+	LNode node = new LNode(e);
+	if (index < 0 || index > size){
+	    return false;}
+	LNode x = head;
+	for (int i = 0; i < index - 1; i++){
+	    x = x.getNext();}
+	x.setNext(node);
+	if(index == size){
+	    tail = node;}
+	size++;
+	return true;
+
+    }
 
     public static void main(String[]args){
 	MyLinkedList test = new MyLinkedList();
 	System.out.println(test);
-	test.add(7);
+	test.add(0);
+	System.out.println(test);
+	test.add(1);
+	System.out.println(test);
+	test.add(3);
+	System.out.println(test);
+	test.add(2,2);
 	System.out.println(test);
     }
 
