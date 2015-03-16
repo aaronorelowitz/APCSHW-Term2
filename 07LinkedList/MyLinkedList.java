523662d2ae0c00f7804e73ced8d30b2ec3f917f2
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-16 13:19:58 -0400

    Update MyLinkedList.java

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -29,6 +29,7 @@ public class MyLinkedList{
 	tail = node;
 	return true;}
     
+    
 
     public boolean add(int index, int e){
 	LNode node = new LNode(e);
@@ -37,13 +38,47 @@ public class MyLinkedList{
 	LNode x = head;
 	for (int i = 0; i < index - 1; i++){
 	    x = x.getNext();}
+	if(index != size){
+	    LNode z = x.getNext();
+	    node.setNext(z);}
 	x.setNext(node);
 	if(index == size){
 	    tail = node;}
 	size++;
 	return true;
-
     }
+
+    public int size(){
+	return size;}
+
+    public LNode get(int index){
+	if (index < 0 || index > size){
+	    return null;}
+	LNode x = head;
+	for (int i = 0; i < index; i++){
+	    x = x.getNext();}
+	return x;
+    }
+
+    
+     public boolean set(int index, int e){
+	 LNode node = new LNode(e);
+	 if (index < 0 || index > size){
+	     return false;}
+	 LNode x = head;
+	 for (int i = 0; i < index - 1; i++){
+	     x = x.getNext();}
+	 LNode z = x.getNext().getNext();
+	 x.setNext(node);
+	 node.setNext(z);
+	 if(index == size){
+	    tail = node;}
+	 size++;
+	 return true;
+     }
+    
+    
+
 
     public static void main(String[]args){
 	MyLinkedList test = new MyLinkedList();
@@ -55,6 +90,15 @@ public class MyLinkedList{
 	test.add(3);
 	System.out.println(test);
 	test.add(2,2);
+	System.out.println(test);
+	test.add(4,4);
+	System.out.println(test);
+	System.out.println(test.get(0));
+	System.out.println(test.get(1));
+	System.out.println(test.get(2));
+	System.out.println(test.get(3));
+	System.out.println(test.get(4));
+	test.set(3,8);
 	System.out.println(test);
     }
 
