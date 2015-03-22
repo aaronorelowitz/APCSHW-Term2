42f561d40d292f3005b419fbf20e43221325f661
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-22 13:47:39 -0400

    Update MyStack.java

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -1,1 +1,16 @@
- 
+ import java.util.*;
+
+public class MyStack{
+    
+    public int[] elements;
+    public int amount;
+    
+    public MyStack(int x){
+	amount = 0;
+	elements = new int[x];
+    }
+
+    
+	
+}
+
