c112c24bff1a5d502426b32036da0090b9c923fb
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-23 20:40:27 -0400

    Update MyQueue.java

--- 09Stacks/MyQueue.java
+++ 09Stacks/MyQueue.java
@@ -1,1 +1,14 @@
- 
+ import java.util.*;
+
+public class MyQueue<E> extends MyLinkedList<E> {
+    
+    public Object dequeue() {
+	return remove(0);
+    }
+
+    public boolean enqueue(Object n){
+	add(n);
+	return true;
+    }
+}
+
