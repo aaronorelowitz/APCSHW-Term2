fbfbdc00ead63ffca14d95b3591e0a5303dffb66
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-03-31 13:13:10 -0400

    Update Maze.java

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -1,1 +1,15 @@
- 
+ import java.util.*;
+import java.io.*;
+
+public class Maze{
+    private char[][]maze;
+    private int maxx,maxy;
+    private int startx,starty;
+    //Terminal keycodes to clear the terminal, or hide/show the cursor
+    private String clear =  "\033[2J";
+    private String hide =  "\033[?25l";
+    private String show =  "\033[?25h";
+    
+    
+    
+}
