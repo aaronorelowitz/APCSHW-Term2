6a3dd6a6763d2d8d0c98a91c7af001e18b38b89f
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-04-02 12:54:28 -0400

    Update Maze.java

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -35,11 +35,31 @@ public class Maze{
 	    e.printStackTrace();
 	    System.exit(0);
 	}
-	
+	maze = new char[maxx][maxy];
+	for(int i = 0; i < ans.length(); i++){
+	    char c = ans.charAt(i);
+	    maze[i % maxx][i / maxx] = c;
+	    if(c == 'S'){
+		startx = i % maxx;
+		starty = i / maxx;
+	    }
+	}
     }
 
     public String toString(){
-	return "blah";
+	String ans = "Solving a maze that is " + maxx + " by " + maxy + "\n";
+	for(int i = 0; i < maxx * maxy; i++){
+	    if(i % maxx == 0 && i != 0){
+		ans += "\n";
+	    }
+	    char c =  maze[i % maxx][i / maxx];
+	    if(c == '#'){
+		ans += color(38,47)+c;
+						}else{
+		ans += color(32,40)+c;
+	    }
+	}
+	return hide + go(0,0) + ans + "\n" + show + color(37,40);
     }
     
     public String toString(boolean animate){
@@ -114,6 +134,15 @@ public class Maze{
 	return ("\033[0;" + foreground + ";" + background + "m");
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
