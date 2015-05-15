f6224b4a2274bcb1e84e7adce2f3f9e377807112
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-05-14 22:21:27 -0400

    Sorry I forgot to commit last night

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -13,20 +13,68 @@ public class MyHeap{
 	Max = isMax;
     }
 
+
+    private void Switch(int x, int y) {
+	int tmp = heap[x];
+	heap[x] = heap[y];
+	heap[y] = tmp;
+    }
+
+    private void SwitchUp(int x){
+	if (x == 1) {
+	    return;
+	} else if (compare(x, x/2)) {
+	    Switch(x, x/2);
+	    SwitchUp(x/2);
+	}
+
+    }  
+
+
+    public boolean compare(int x, int y){
+	if(Max){
+	    return (x > y);}
+	else{return (x < y);}
+    }
+
+    
+    
     public void resize(){
 	int[]newHeap = new int[heap.length * 2];
 	for(int i = 0; i < heap.length; i++){
 	    newHeap[i] = heap[i];}
 	heap = newHeap;
     }
+
+    public int size(){
+	return heap[0];}
     
     public void add(int num){
 	heap[0] = heap[0] + 1;
-	if(heap.length <= heap[0]){
+	if(heap.length <= size()){
 	    resize();}
-	heap[heap[0]] = num;
-	}
+	heap[size()] = num;
+	SwitchUp(size());
     }
+    
+    public int remove() {
+	int tmp = heap[1];
+	heap[1] = heap[size()];
+        SwitchDown(1);
+	heap[0]= heap[0] - 1;
+	return tmp;
+    }
+    
+    private void SwitchDown(int x) {
+	if (x*2 > size()) {
+	    return;
+	}
+	if (!compare(x, x*2)) {
+	    Switch(x, x*2);
+	    SwitchDown(x*2);
+	}
+  }
+
 
 
 }
