bfb31e4314380ab7c47c4d4c31df00fd842d721a
aaronorelowitz <aaronorelowitz1998@gmail.com>
2015-05-17 16:18:37 -0400

    Update RunningMedian.java

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -8,8 +8,19 @@ public class RunningMedian{
 	MaxHeap = new MyHeap(true);}
 
     public Sting name(){
-	return "orelowitz.aaron"}
+	return "aaron.orelowitz"}
 
+    
 
+    public int getMedian(){
+	if(MinHeap.size() == 0 && MaxHeap.size() == 0){
+	    return 0;}
+	if(MinHeap.size() > MaxHeap.size()){
+	    return MinHeap.peak();}
+	if(MaxHeap.size() > MinHeap.size()){
+	    return MaxHeap.peak();}
+	else{
+	    MaxHeap.peak() + MinHeap.peak() / 2
+	}
+    }
 }
-
